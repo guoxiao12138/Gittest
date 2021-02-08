5b0460c6e531017942e17d6db591d9271bc63ed9
guoxiao <Administrator@AB3778WHJ0CLYFH>
2021-02-08 19:11:06 +0800

    创建分支

--- src/main/java/com/guoxiao/git/HElloGit.java
+++ src/main/java/com/guoxiao/git/HElloGit.java
@@ -5,7 +5,9 @@ public class HElloGit {
 	public static void main(String[] args) {
 	System.out.println("Hello Git");
 	System.out.println("更新1");
-
+	System.out.println("创建分支1");
+	System.out.println("创建分支2");
+	System.out.println("主线添加");
 	}
 
 }
